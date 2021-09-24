915bfb95a77f96a0d109888fbd4a76053412083d
Julien Dubois <julien.dubois@gmail.com>
2021-09-24 10:39:06 +0200

    Code controller

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,2 +1,13 @@
-package com.example.demo;public class HelloController {
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    String hello() {
+        return "Hello from Spring";
+    }
 }
